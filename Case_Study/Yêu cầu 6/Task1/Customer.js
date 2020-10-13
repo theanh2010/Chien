7fe187b34471fabb4063eeb479807924fd2a7607
C0920G1-Tran-Minh-Chien <chiennext@gmail.com>
2020-10-13 16:17:24 +0700

    up

--- Case_Study/Yêu cầu 6/Task1/Customer.js
+++ Case_Study/Yêu cầu 6/Task1/Customer.js
@@ -1,90 +1,128 @@
 
-let Customer = function () {
-        this.setName = function (name) {
-            this.name = name;
-        }
-        this.getName = function () {
-            return this.name;
-        }
-
-        this.setCmnd = function (cmnd) {
-            this.cmnd = cmnd;
-        }
-        this.getCmnd = function () {
-            return this.cmnd;
-        }
-
-        this.setBirthday = function (birthday) {
-            this.birthday = birthday;
-        }
-        this.getBirthday = function () {
-            return this.birthday;
-        }
-
-        this.setEmail = function (email) {
-            this.email = email;
-        }
-        this.getEmail = function () {
-            return this.email;
-        }
-
-        this.setAddress = function (address) {
-            this.address = address;
-        }
-        this.getAddress = function () {
-            return this.address;
-        }
-
-        this.setTypeService = function (typeService) {
-            this.typeService = typeService;
-        }
-        this.getTypeService = function () {
-            return this.typeService;
-        }
-
-        this.setTypeRoom = function (typeRoom) {
-            this.typeRoom = typeRoom;
-        }
-        this.getTypeRoom = function () {
-            return this.typeRoom;
-        }
-
-        this.setTypeCustomer = function (typeCustomer) {
-            this.typeCustomer = typeCustomer;
-        }
-        this.getTypeCustomer = function () {
-            return this.typeCustomer;
-        }
-
-        this.setDiscount = function (discount) {
-            this.discount = discount;
-        }
-        this.getDiscount = function () {
-            return this.discount;
-        }
-
-        this.setAmount = function (amount) {
-            this.amount = amount;
-        }
-        this.getAmount = function () {
-            return this.amount;
-        }
-
-        this.setRentDays = function (rentDays) {
-            this.rentDays = rentDays;
-        }
-        this.getRentDays = function () {
-            return this.rentDays;
-        }
-        this.totalPays = function () {
-            let money = 0;
-            if (this.getTypeService() === "Villa" ){
-                money = 500;
-            } else if (this.getTypeService() === "House"){
-                money = 300;
-            } else if (this.getTypeService() === "Room"){
-                money = 100;
-            }
-            return this.getRentDays()*money*(1-this.getDiscount()/100);
-        }
+class Customer {
+    constructor(name, cmnd, birthDay, address, email, typeService, typeRoom, typeCustomer, discount, amount, rentDays, totalPays) {
+        this.name = name;
+        this.cmnd = cmnd;
+        this.birthDay = birthDay;
+        this.address = address;
+        this.email = email;
+        this.typeService = typeService;
+        this.typeRoom = typeRoom;
+        this.typeCustomer = typeCustomer ;
+        this.discount = discount;
+        this.amount = amount;
+        this.rentDays = rentDays;
+        this.totalPays = totalPays ;
     }
+    setName (name){
+        this.name = name;
+    };
+    setCmnd (cmnd){
+        this.cmnd = cmnd;
+    };
+    setBirthDay (birthDay){
+        this.birthDay = birthDay;
+    };
+    setAddress (address){
+        this.address = address;
+    };
+    setEmail (email){
+        this.email = email;
+    };
+    setTypeService (typeService){
+        this.typeService = typeService;
+    };
+    setTypeRoom (typeRoom){
+        this.typeRoom = typeRoom;
+    };
+    setTypeCustomer (typeCustomer) {
+        this.typeCustomer = typeCustomer;
+    };
+    setDiscount (discount){
+        this.discount = discount;
+    };
+    setAmount (amount){
+        this.amount = amount;
+    };
+    setRentDays (rentDays){
+        this.rentDays = rentDays;
+    };
+    setTotalPays (totalPays) {
+        this.totalPays = totalPays;
+    }
+
+    getName (){
+        return this.name;
+    };
+    getCmnd (){
+        return this.cmnd;
+    };
+    getBirthDay (){
+        return this.birthDay;
+    };
+    getAddress (){
+        return this.address;
+    };
+    getEmail (){
+        return this.email;
+    };
+    getTypeService (){
+        return this.typeService;
+    };
+    getTypeRoom (){
+        return this.typeRoom;
+    };
+    getTypeCustomer (){
+        return this.typeCustomer;
+    };
+    getDiscount (){
+        return this.discount;
+    };
+    getRentDays (){
+        return this.rentDays;
+    };
+    getAmount (){
+        return this.amount;
+    };
+
+    getTotalPays(){
+            let money1 = 0 ;
+            if (this.getTypeService() === "Villa" ){
+                money1 = 500;
+            } else if (this.getTypeService() === "House"){
+                money1 = 300;
+            } else if (this.getTypeService() === "Room"){
+                money1 = 100;
+            }
+
+            let money2 = 0;
+            if (this.getAddress() === "Đà Nẵng" ){
+                money2 = 50;
+            } else if (this.getAddress() === "Huế"){
+                money2 = 30;
+            } else if (this.getAddress() === "Quảng Nam"){
+                money2 = 10;
+            }
+
+            let money3 = 0;
+            if (this.getTypeCustomer() === "Diamond" ){
+                money3 = 50;
+            } else if (this.getTypeService() === "Platium"){
+                money3 = 30;
+            } else if (this.getTypeService() === "Gold"){
+                money3 = 10;
+            }
+
+            let money4 = 0;
+            if (this.getRentDays() > 7){
+                money4 = 50;
+            } else if (this.getRentDays() > 5){
+                money4 = 30;
+            } else if (this.getRentDays() > 2){
+                money4 = 10;
+            }
+
+            this.totalPays = this.getRentDays()*money1*(1-this.getDiscount()/100)  - money2 - money3 - money4;
+            return this.totalPays;
+        }
+}
